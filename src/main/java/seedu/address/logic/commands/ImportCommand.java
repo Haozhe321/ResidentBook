04c7b8360571778aef166f2db018f02684f2d9f4
glenice <glenicetan@hotmail.com>
2017-10-18 20:40:46 +0800

    Add import function

---
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.commands;
+
+import javafx.collections.ObservableList;
+import seedu.address.MainApp;
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.UndoableCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Import contacts from xml file.
+ */
+public class ImportCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "import";
+    public static final String COMMAND_ALIAS = "i";
+    public static final String MESSAGE_SUCCESS = "Import successful.";
+    public static final String MESSAGE_ERROR = "Import error.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds all persons in the XML file onto "
+            + "the current address book.\n"
+            + "Parameters: FILE_PATH \n"
+            + "Example: " + COMMAND_WORD + " friendsContacts.xml";
+
+    private String filePath;
+
+    public ImportCommand(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    protected CommandResult executeUndoableCommand() throws CommandException {
+        requireNonNull(model);
+        try {
+            Optional<ReadOnlyAddressBook> newContacts = MainApp.getBackup().readAddressBook(filePath);
+            ReadOnlyAddressBook newList = newContacts.orElse(null);
+
+            if (newList != null) {
+                ObservableList<ReadOnlyPerson> personList = newList.getPersonList();
+
+                for (ReadOnlyPerson p : personList) {
+                    model.addPerson(p);
+                }
+            }
+            return new CommandResult(String.format(MESSAGE_SUCCESS));
+        } catch (DataConversionException e) {
+            throw new CommandException(MESSAGE_ERROR);
+        } catch (IOException e) {
+            throw new CommandException(MESSAGE_ERROR);
+        } catch (DuplicatePersonException e) {
+            throw new CommandException(AddCommand.MESSAGE_DUPLICATE_PERSON);
+        }
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.Import;
+import seedu.address.logic.commands.ImportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new ImportCommand object
+ */
+public class ImportCommandParser implements Parser<ImportCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the ImportCommand
+     * and returns an ImportCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+
+    public ImportCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE));
+        }
+        return new ImportCommand(trimmedArgs);
+    }
+}
